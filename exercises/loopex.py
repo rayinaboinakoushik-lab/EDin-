31f21085249d55cfb00c2b1384ee1930a3bd40eb
Koushik <rayinaboinakoushik@gmail.com>
2025-11-14 23:08:13 +0100

    json operations

--- exercises/loopex.py
+++ exercises/loopex.py
@@ -1,3 +1,8 @@
+import json 
 videos=[{"id":"a1","title":"intro"},{"id":"a2","title":"transcripts"}]
-for video in videos:
+for i,video in enumerate(videos):
     print(video['id'],'-',video['title'])
+    with open('Trial.json','r',encoding='utf-8') as trial:
+     data=json.load(trial)
+    print(data)
+    final=json.dumps(data,indent=2)
