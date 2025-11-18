e46af94f82e973f1b64449d751af85beecad0d29
Koushik <rayinaboinakoushik@gmail.com>
2025-11-18 20:22:30 +0100

    exercises

---
+++ exercises/functions.py
@@ -0,0 +1,11 @@
+data=['a','3','26']
+def fun(name):
+ return data
+ print (name)
+ 
+    
+value=fun(data)
+fun("ved")
+print(value)
+
+
--- exercises/loopex.py
+++ exercises/loopex.py
@@ -1,8 +1,12 @@
-import json 
+import json
+with open('Trial.json','r',encoding='utf-8') as trial:
+      data=json.load(trial)
+
 videos=[{"id":"a1","title":"intro"},{"id":"a2","title":"transcripts"}]
 for i,video in enumerate(videos):
-    print(video['id'],'-',video['title'])
-    with open('Trial.json','r',encoding='utf-8') as trial:
-     data=json.load(trial)
-    print(data)
-    final=json.dumps(data,indent=2)
+    print(video['id'],'-',video['title'])         
+    
+print(data)
+
+
+    
